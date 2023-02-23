68f52eb346efa027c8abbcbff2c46c44b0e81383
Mattias Holm <lorrden@openorbit.org>
2023-02-23 06:03:33 +0100

    Add documentation comments and license header for resolver

--- Sources/Sim/Resolver.swift
+++ Sources/Sim/Resolver.swift
@@ -1,12 +1,28 @@
 //
-//  File.swift
-//  
+// SPDX-License-Identifier: Apache-2.0
 //
-//  Created by Mattias Holm on 2023-02-22.
+// Copyright 2023 Mattias Holm
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 import Foundation
 
+/// Resolves simulation paths
+/// Paths can be either absolute (starting with /) or relative with respect to a model.
+/// Looking for /foo/bar will return the submodel of foo called bar.
+/// Path expressions resolve to models, not fields in the models.
+/// Path expressions support `..` and `.` meaning parent or this model.
 public class Resolver {
   weak var sim: Simulator!
   init(sim: Simulator)
@@ -14,6 +30,9 @@ public class Resolver {
     self.sim = sim
   }
 
+  /// Resolve absolute path in the simulation tree
+  /// - Parameter absolute: Absolute path starting with `/`
+  /// - Returns: Returns the model at the given path in the simulation tree, or nil if not found
   public func resolve(absolute: String) -> Model? {
     let components = absolute.split(separator: "/")
 
@@ -31,6 +50,11 @@ public class Resolver {
     return cursor
   }
 
+  /// Resolve relative path, starting at the given model instead of the root
+  /// - Parameters:
+  ///   - relative: Relative path starting with a model name, `..` or `.`
+  ///   - source: Source model
+  /// - Returns: Model at the relative path location, or nil if not found.
   public func resolve(relative: String, source: Model) -> Model? {
     let components = relative.split(separator: "/")
     var cursor: Model? = source
